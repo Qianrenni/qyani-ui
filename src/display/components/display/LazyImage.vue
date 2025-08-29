fb30585e43627a9465a34a61d43e4e31776dd1b1
qianrenni <15133897+Qianrenni@user.noreply.gitee.com>
2025-08-29 19:16:34 +0800

    feat:更新1.3.0

--- src/display/components/display/LazyImage.vue
+++ src/display/components/display/LazyImage.vue
@@ -1,22 +1,64 @@
 <template>
   <DemoBlock :code="code">
-    <QLazyImage :src="`https://picsum.photos/300/400`"
-                :height="400"
-                :width="300"
-    />
+    <QScrollContainer
+        :threshold="200"
+        @ended="add"
+        class="container-w100 container container-wrap"
+        scoll-y
+        style="height: 600px;width: 1008px;"
+    >
+      <QLazyImage
+          v-for="item in photos" :key="item" :src="item"
+          :height="100"
+          :width="100"
+      >
+
+      </QLazyImage>
+    </QScrollContainer>
 
   </DemoBlock>
 </template>
 <script lang="ts" setup>
 import {QLazyImage} from "qyani-components";
+import {QScrollContainer} from "qyani-components";
 import DemoBlock from "@/DemoBlock.vue";
+import {ref} from "vue";
 
+const photos = ref(Array.from({length:100},(_,i)=>`https://picsum.photos/100?random=${i}`))
+
+const add = () => {
+  console.log('end');
+  photos.value.push(...Array.from({length:10},(_,i)=>`https://picsum.photos/100?random=${photos.value.length+i}`))
+}
 const code =`
 \`\`\`html
-<QLazyImage :src="\`https://picsum.photos/300/400\`"
-            :height="400"
-            :width="300"
-/>
-\`\`\``
+    <QScrollContainer
+        :threshold="200"
+        @ended="add"
+        class="container-w100 container container-wrap"
+        scoll-y
+        style="height: 600px;width: 1008px;"
+    >
+      <QLazyImage
+          v-for="item in photos" :key="item" :src="item"
+          :height="100"
+          :width="100"
+      >
+
+      </QLazyImage>
+    </QScrollContainer>
+\`\`\`
+`+`
+\`\`\`ts
+import {ref} from "vue";
+
+const photos = ref(Array.from({length:100},(_,i)=>\`https://picsum.photos/100?random=\${i}\`))
+
+const add = () => {
+  console.log('end');
+  photos.value.push(...Array.from({length:10},(_,i)=>\`https://picsum.photos/100?random=\${photos.value.length + i}\`))
+}
+\`\`\`
+`
 
 </script>
